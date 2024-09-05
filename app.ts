9ded482561399ce500a9e1e2e285684d6852bb39
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 20:36:14 +0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,22 @@
+let a: number = 5;
+switch (a) {
+    case 1:
+        console.log("Sayın 1")
+        break;
+    case 2:
+        console.log("Sayın 2")
+        break;
+    case 3:
+        console.log("Sayın 3")
+        break;
+    case 4:
+        console.log("Sayın 4")
+        break;
+    case 5:
+        console.log("Sayın 5")
+        break;
+    default:
+        console.log("Sayın Burada Yok")
+        break;
+
+}
